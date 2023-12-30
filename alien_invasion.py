0a24a088148058f58e02b3570c7d1674ffedeaf5
Leonardo Alberto Zuñiga Villavazo <leo.zuniga@ibm.com>
2023-12-29 18:34:46 -0600

    Update alien_invasion.py
    Make the screen game fullscreen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,8 +10,9 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-    
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien invasion")
 
         self.ship = Ship(self)
