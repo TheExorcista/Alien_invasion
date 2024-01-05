8a7ea1d47fe0a8a700176fa35cfb160d3d53fe5d
Leonardo Alberto Zuñiga Villavazo <leo.zuniga@ibm.com>
2024-01-04 19:43:53 -0600

    Bullet module added
    - Events added into alien_invasion.py
    - Bullet color added in the settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behaivor"""
@@ -16,12 +17,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
            
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()                  
             self.clock.tick(60)
     
@@ -42,6 +45,13 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _check_keyup_events(self,event):
         """Respond to key releases."""
@@ -54,6 +64,8 @@ class AlienInvasion:
     def _update_screen(self):
         """Update images on the screen and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
 
