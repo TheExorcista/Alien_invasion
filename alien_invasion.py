0f167b6ddf2c3411c743fe888f8651d179e09d89
Leonardo Alberto Zuñiga Villavazo <leo.zuniga@ibm.com>
2024-01-11 21:55:25 -0600

    Alien class creation
    Alien class creation and added to alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behaivor"""
@@ -18,6 +19,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
            
     def run_game(self):
         """Start the main loop for the game."""
@@ -27,6 +31,13 @@ class AlienInvasion:
             self.bullets.update()
             self._update_screen()                  
             self.clock.tick(60)
+    
+    def _create_fleet(self):
+        """Create a flet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -67,6 +78,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
+        
         pygame.display.flip()
 
 if __name__ == '__main__':
